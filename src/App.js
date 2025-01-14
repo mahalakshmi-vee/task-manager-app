b961a861797cba8f36d30a92dfda3c4a5772fbe8
Mahalakshmi Veeraraj <mahalakshmi.veeraraj.scaler@gmail.com>
2025-01-14 17:39:56 +0530

    Add App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import logo from "./logo.svg";
-import "./App.css";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
 import AppRouter from "./components/AppRouter.js";
